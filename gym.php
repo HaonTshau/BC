4734852e96c430a8cd47b552c22b94e26e966f2b
HaonTshau <250268579@qq.com>
2017-12-16 01:48:30 +0800

    v1.1

--- gym.php
+++ gym.php
@@ -4,12 +4,12 @@ try{
 	$bc = new PDO('mysql:host=localhost;dbname=bc', USER, PASSWORD);
 	$bc->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	$bc->exec("SET CHARACTER SET utf8");
-	//$bc->exec("SET GLOBAL sql_mode = ''");
 }catch(PDOException$e){
 	print"Error!:".$e->getMessage()."<br/>";
 	die();
 }
 //////////////////////////////////////////
+//global var
 $classSelected = isset($_GET['classSelected']) ? $_GET['classSelected'] : '';
 $timeSelected = isset($_GET['timeSelected']) ? $_GET['timeSelected'] : '';
 $name = isset($_GET['name_']) ? $_GET['name_'] : '';
@@ -40,7 +40,6 @@ $result = $bc->query($sql_getTime);
 $tempArray = array();
 $Time_ = '<option value=""></option>';
 while($row = $result->fetch(PDO::FETCH_ASSOC)){
-	
 		array_push($tempArray,$row['time']);
 		if(in_array($timeSelected,$tempArray,true)){
 			$Time_ = $Time_.'<option value="'.$row['time'].'" selected="selected">'.$row['time'].'</option>';
@@ -48,7 +47,6 @@ while($row = $result->fetch(PDO::FETCH_ASSOC)){
 		else{
 			$Time_ = $Time_.'<option value="'.$row['time'].'">'.$row['time'].'</option>';
 		}
-	
 }
 unset($tempArray);
 ?>
@@ -57,66 +55,56 @@ unset($tempArray);
 <head>
 <title>Book Class</title>
 <meta charset="utf-8" />
+<script>
+function getTimeList(){
+var url = '?classSelected='+document.bookclass.classSelected.value+'&name_='+document.bookclass.name_.value+'&number_='+document.bookclass.number_.value;
+window.location.href = url;
+}
+</script>
 </head>
 <body>
 <div style="width:51%;height:320px;margin:0 auto;padding:0;">
-<img style="margin-left: 25px;" src="class.png" />
+	<img style="margin-left: 25px;" src="class.png" />
 </div>
 <div style="width:911px;height:320px;margin:0 auto;padding:0;margin-top:30px;">
-<form name="bookclass" action="bookclass.php" method="get">
-<table border=1 cellpadding="" style="border-color:black;width:100%;">
-<tr>
-	<td>Class</td>
-	<td>
-		<select id="classSelected" name="classSelected" style="width:300px;" onchange="getTimeList()">
-			<?php echo $Class_?>
-		</select>
-	</td>
-</tr>
-<tr>
-	<td>Times</td>
-	<td>
-		<select id="timeSelected" name="timeSelected" style="width:300px;">
-			<?php echo $Time_?>
-		</select>
-	</td>
-</tr>
-<tr>
-	<td>Name</td>
-	<td>
-		<input id="name" style="width:300px;" type="text" name="name_" value="<?php echo $name?>" />
-	</td>
-</tr>
-<tr>
-	<td>Phone or mobile number</td>
-	<td>
-		<input id="number" style="width:300px;" type="text" name="number_" value="<?php echo $number?>" /><br />
-	</td>
-</tr>
-<tr>
-<td></td>
-<td>
-<button id="submit_" type="submit" style="width:100px;height:35px;" value="submit">Submit</button>
-</td>
-</tr>
-
-</table>
-</form>
-
+	<form name="bookclass" action="bookclass.php" method="get">
+		<table border=1 cellpadding="" style="border-color:black;width:100%;">
+			<tr>
+				<td>Class</td>
+				<td>
+					<select id="classSelected" name="classSelected" style="width:300px;" onchange="getTimeList()">
+						<?php echo $Class_?>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Times</td>
+				<td>
+					<select id="timeSelected" name="timeSelected" style="width:300px;">
+						<?php echo $Time_?>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Name</td>
+				<td>
+					<input id="name" style="width:300px;" type="text" name="name_" value="<?php echo $name?>" />
+				</td>
+			</tr>
+			<tr>
+				<td>Phone or mobile number</td>
+				<td>
+					<input id="number" style="width:300px;" type="text" name="number_" value="<?php echo $number?>" /><br />
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>
+					<button id="submit_" type="submit" style="width:100px;height:35px;" value="submit">Submit</button>
+				</td>
+			</tr>
+		</table>
+	</form>
 </div>
-
 </body>
 </html>
-<script>
-
-function getTimeList(){
-		//alert(document.bookclass.course.value);
-
-var url = '?classSelected='+document.bookclass.classSelected.value+'&name_='+document.bookclass.name_.value+'&number_='+document.bookclass.number_.value;
-
-window.location.href = url;
-}
-
-
-
-</script>
